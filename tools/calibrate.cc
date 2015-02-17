8e6bf74b3e998af8f304c4b847fc3cead3a9e097
Christian Kellner <christian@kellner.me>
2015-02-17 18:49:40 +0100

    [tools] calib: add skeleton for calibration tool

---
+++ tools/calibrate.cc
@@ -0,0 +1,85 @@
+#define GLFW_INCLUDE_GLCOREARB
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <string>
+
+static void error_callback(int error, const char* description)
+{
+    std::cerr << description << std::endl;
+}
+
+static void check_gl_error(const std::string &prefix = "") {
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+        std::cerr << prefix << "OpenGL error: " << err << std::endl;
+    }
+}
+
+void wnd_key_cb(GLFWwindow *wnd, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(wnd, GL_TRUE);
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+    GLFWmonitor *primary = glfwGetPrimaryMonitor();
+
+    std::cout << glfwGetMonitorName(primary) << std::endl;
+
+    int n_modes;
+    const GLFWvidmode* modes = glfwGetVideoModes(primary, &n_modes);
+
+    for (int i = 0; i < n_modes; i++) {
+        std::cout << i << ": " << modes[i].width << " x " << modes[i].height;
+        std::cout << " \t " << modes[i].redBits << " " << modes[i].greenBits << " " << modes[i].blueBits;
+        std::cout << " @ " << modes[i].refreshRate << "Hz" << std::endl;
+    }
+
+    const GLFWvidmode *mm = modes + (n_modes - 1);
+    GLFWwindow* window = glfwCreateWindow(mm->width, mm->height, "Calibration", primary, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, wnd_key_cb);
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
+    glEnable(GL_MULTISAMPLE);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
+
+}
