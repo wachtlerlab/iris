5112ecfc7d5167583d02fe5d29c91a5dc6ad7b71
Christian Kellner <christian@kellner.me>
2015-03-01 17:01:39 +0100

    [glue] basic: point, extent, rect

---
+++ lib/glue/basic.h
@@ -0,0 +1,57 @@
+
+#ifndef GLUE_BASIC_H
+#define GLUE_BASIC_H
+
+namespace glue {
+
+struct point {
+
+    float x;
+    float y;
+
+    point() : x(0.0f), y(0.0f) {}
+    point(float x, float y) : x(x), y(y) { }
+};
+
+
+struct extent {
+    float width;
+    float height;
+
+    extent() : width(0.0f), height(0.0f) {}
+    extent(float width, float height) : width(width), height(height) { }
+    extent(int width, int height) : width(1.0f * width), height(1.0f * height) { }
+};
+
+
+struct rect {
+
+    union {
+        struct {
+            point start;
+            extent size;
+        };
+        struct {
+            float x;
+            float y;
+            float width;
+            float height;
+        };
+    };
+
+    rect() : start(), size() {}
+    rect(float x, float y, float width, float height)
+            : start(x, y), size(width, height) {
+    }
+
+
+    bool is_inside(float x, float y) const {
+        return x > start.x && x < start.x + size.width && y > start.y && y < start.y + size.height;
+    }
+
+};
+
+
+}
+
+#endif
