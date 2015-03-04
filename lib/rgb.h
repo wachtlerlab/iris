ed978add9e8014c637266f6a5032a4da470056bc
Christian Kellner <christian@kellner.me>
2015-03-04 15:48:01 +0100

    rgb: add "basic" rgb colors

--- lib/rgb.h
+++ lib/rgb.h
@@ -18,6 +18,16 @@ struct rgb {
         float raw[3];
     };
 
+    static constexpr rgb white() { return rgb(1.0f, 1.0f, 1.0f);}
+    static constexpr rgb black() { return rgb(0.0f, 0.0f, 0.0f); }
+    static constexpr rgb gray(float level = 0.5f) { return rgb(level, level, level); }
+    static constexpr rgb red(float level = 1.0f) { return rgb(level, 0.0f, 0.0f); }
+    static constexpr rgb green(float level = 1.0f) { return rgb(0.0f, level, 0.0f); }
+    static constexpr rgb blue(float level = 1.0f) { return rgb(0.0f, 0.0f, level); }
+    static constexpr rgb yellow(float level = 1.0f) { return rgb(level, level, 0.0f); }
+    static constexpr rgb cyan(float level = 1.0f) { return rgb(0.0f, level, level); }
+    static constexpr rgb magenta(float level = 1.0f) { return rgb(level, 0.0f, level); }
+
 };
 
 }
