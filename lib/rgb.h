f1293bd0a900b6aca47632a3b4e0625d27d35b9c
Christian Kellner <christian@kellner.me>
2015-03-04 15:05:55 +0100

    rgb: simple rgb abstraction for general lib

---
+++ lib/rgb.h
@@ -0,0 +1,25 @@
+#ifndef IRIS_RGB_H
+#define IRIS_RGB_H
+
+namespace iris {
+
+struct rgb {
+
+    rgb() : r(0.0f), g(0.0f), b(0.0f) {}
+    rgb(float r, float g, float b) : r(r), g(g), b(b) {}
+
+    union {
+        struct {
+            float r;
+            float g;
+            float b;
+        };
+
+        float raw[3];
+    };
+
+};
+
+}
+
+#endif
