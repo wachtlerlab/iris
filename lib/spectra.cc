e710032f0ef58f29a5af01e3a1e41179a17e1d86
Christian Kellner <christian@kellner.me>
2015-03-06 13:23:39 +0100

    spectra: move non-trivial impl to .cc file

---
+++ lib/spectra.cc
@@ -0,0 +1,98 @@
+
+#include <spectra.h>
+
+namespace iris {
+
+// ***********
+// spectrum
+
+float spectrum::operator*(const spectrum &other) {
+    if (other.wl_start != wl_start || other.wl_step != wl_step ||
+                other.values.size() != values.size()) {
+            throw std::invalid_argument("Incompatible spectra");
+        }
+
+        double vsum = 0;
+        for(size_t i = 0; i < values.size(); i++) {
+            double m = values[i] * other.values[i];
+            vsum += m;
+        }
+
+        return static_cast<float>(vsum);
+ }
+
+spectrum spectrum::operator+(const spectrum &other) {
+    if (other.wl_start != wl_start || other.wl_step != wl_step ||
+            other.values.size() != values.size()) {
+        throw std::invalid_argument("Incompatible spectra");
+    }
+
+    spectrum res;
+    res.wl_start = wl_start;
+    res.wl_step = wl_step;
+    res.id = id;
+    res.values.resize(values.size());
+
+    for(size_t i = 0; i < values.size(); i++) {
+        res.values[i] = values[i] + other.values[i];
+    }
+
+    return res;
+}
+
+
+// ***********
+// spectra
+
+ssize_t spectra::find_spectrum(const std::string &id) const {
+
+    bool found = false;
+    size_t i;
+    for (i = 0; i < ids.size(); i++) {
+        if (id == ids[i]) {
+            found = true;
+            break;
+        }
+    }
+
+    return found ? i : -1;
+}
+
+spectrum spectra::operator[](size_t n) const {
+    if (n > n_spectra) {
+        throw std::out_of_range("spectrum requested is oor");
+    }
+
+    spectrum s(wl_start, wl_step);
+
+    if (!ids.empty() && ids.size() >= n) {
+        s.name(ids[n]);
+    }
+
+    s.resize(n_samples);
+    float *ptr = storage + (n * n_samples);
+    std::memcpy(s.data(), ptr, sizeof(float) * n_samples);
+    return s;
+}
+
+spectrum spectra::operator[](const std::string &name) const {
+    ssize_t pos = find_spectrum(name);
+    if (pos < 0) {
+        return spectrum();
+    }
+
+    return this->operator[](static_cast<size_t>(pos));
+}
+
+void spectra::allocate() {
+    size_t n = n_spectra * n_samples;
+    if (n < 1) {
+        storage = nullptr;
+        return;
+    }
+
+    std::allocator<float> al;
+    storage = al.allocate(n);
+}
+
+} // iris::
--- lib/spectra.h
+++ lib/spectra.h
@@ -18,39 +18,8 @@ public:
                              values(std::move(o.values)), id(std::move(o.id)) {
     }
 
-    float operator*(const spectrum &other) {
-        if (other.wl_start != wl_start || other.wl_step != wl_step ||
-                other.values.size() != values.size()) {
-            throw std::invalid_argument("Incompatible spectra");
-        }
-
-        double vsum = 0;
-        for(size_t i = 0; i < values.size(); i++) {
-            double m = values[i] * other.values[i];
-            vsum += m;
-        }
-
-        return static_cast<float>(vsum);
-    }
-
-    spectrum operator+(const spectrum &other) {
-        if (other.wl_start != wl_start || other.wl_step != wl_step ||
-                other.values.size() != values.size()) {
-            throw std::invalid_argument("Incompatible spectra");
-        }
-
-        spectrum res;
-        res.wl_start = wl_start;
-        res.wl_step = wl_step;
-        res.id = id;
-        res.values.resize(values.size());
-
-        for(size_t i = 0; i < values.size(); i++) {
-            res.values[i] = values[i] + other.values[i];
-        }
-
-        return res;
-    }
+    float operator*(const spectrum &other);
+    spectrum operator+(const spectrum &other);
 
     float& operator[](size_t n) {
         return values[n];
@@ -154,45 +123,11 @@ public:
         return wl_step;
     }
 
-    ssize_t find_spectrum(const std::string &id) const {
+    ssize_t find_spectrum(const std::string &id) const;
 
-        bool found = false;
-        size_t i;
-        for (i = 0; i < ids.size(); i++) {
-            if (id == ids[i]) {
-                found = true;
-                break;
-            }
-        }
+    spectrum operator[](size_t n) const;
 
-        return found ? i : -1;
-    }
-
-    spectrum operator[](size_t n) const {
-        if (n > n_spectra) {
-            throw std::out_of_range("spectrum requested is oor");
-        }
-
-        spectrum s(wl_start, wl_step);
-
-        if (!ids.empty() && ids.size() >= n) {
-            s.name(ids[n]);
-        }
-
-        s.resize(n_samples);
-        float *ptr = storage + (n * n_samples);
-        std::memcpy(s.data(), ptr, sizeof(float) * n_samples);
-        return s;
-    }
-
-    spectrum operator[](const std::string &name) const {
-        ssize_t pos = find_spectrum(name);
-        if (pos < 0) {
-            return spectrum();
-        }
-
-        return this->operator[](static_cast<size_t>(pos));
-    }
+    spectrum operator[](const std::string &name) const;
 
     void names(std::vector<std::string> data) {
         ids = std::move(data);
@@ -203,16 +138,7 @@ public:
     }
 
 private:
-    void allocate() {
-        size_t n = n_spectra * n_samples;
-        if (n < 1) {
-            storage = nullptr;
-            return;
-        }
-
-        std::allocator<float> al;
-        storage = al.allocate(n);
-    }
+    void allocate();
 
 private:
     float *storage;
