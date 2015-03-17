00e93c0b34c3dacf4c0051c166c5046e359e2aba
Christian Kellner <christian@kellner.me>
2015-03-17 17:00:05 +0100

    [plot] sine-fit, draw midline
    phase is substracted

--- plot/iris-ps.py
+++ plot/iris-ps.py
@@ -10,13 +10,14 @@ import sys
 import csv
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(description='CI - Configuration Tool')
-    parser.add_argument('data', type=str)
-    parser.add_argument('--offset,-o', dest='offset', type=float, default=0.66)
     parser.add_argument('--freq', dest='freq', type=float, default=1.0)
+    parser.add_argument('data', type=str)
     parser.add_argument('amp', type=float, default=None)
     parser.add_argument('phase', type=float, default=None)
+    parser.add_argument('offset', type=float, default=0.66)
 
     args = parser.parse_args()
     x = []
@@ -28,6 +29,7 @@ def main():
             y.append(float(row[1]))
 
     plt.scatter(x, y,  color='dodgerblue', label='data')
+    plt.axhline(y=args.offset, color='black')
 
     phase = args.phase
     amp = args.amp
@@ -35,7 +37,7 @@ def main():
     if phase is not None and amp is not None:
         x = np.array(sorted(set(x)))
         plt.hold(True)
-        yfit = args.offset + amp * np.sin(args.freq*x + phase)
+        yfit = args.offset + amp * np.sin(args.freq*x - phase)
         plt.plot(x, yfit, '--k')
 
     plt.show()
