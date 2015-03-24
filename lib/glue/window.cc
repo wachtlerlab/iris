6056f8eb11805d0c43a76b7c5d4a8acf0a76d1dd
Christian Kellner <christian@kellner.me>
2015-03-24 15:09:12 +0100

    [glue] window: do ask for gl 3.2 in mac

--- lib/glue/window.cc
+++ lib/glue/window.cc
@@ -35,7 +35,7 @@ GLFWwindow* window::make(int height, int width, const std::string &title, monito
 #ifndef __APPLE__
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 #else
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
