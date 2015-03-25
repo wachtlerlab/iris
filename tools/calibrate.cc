621dbd47ccf0646565f05e284a84087fe9c20d28
Christian Kellner <christian@kellner.me>
2015-03-25 13:57:07 +0100

    [tools] calibrate: check luminance option

--- tools/calibrate.cc
+++ tools/calibrate.cc
@@ -36,6 +36,39 @@ static void dump_sepctra(const iris::spectra &spec) {
     }
 }
 
+std::vector<double> cmp_luminance(const h5x::File &fd, const iris::spectra &cf, const iris::spectra &spec)
+{
+    iris::spectrum L = cf[1];
+    iris::spectrum M = cf[2];
+
+    iris::spectrum Lumeff = L+M;
+
+    h5x::DataSet ls = fd.openData("luminance");
+    h5x::NDSize ls_size = ls.size();
+    std::vector<double> lum_meter(ls_size[0]);
+    ls.read(h5x::TypeId::Double, ls_size, lum_meter.data());
+
+    std::vector<double> res(ls_size[0], 0);
+
+    std::cerr << "measure \t calc \t Δ " << std::endl;
+
+    for (size_t p = 0; p < spec.num_spectra(); p++) {
+        iris::spectrum sp = spec[p];
+
+        double lc = (Lumeff * sp).integrate();
+        double delta =  lc - lum_meter[p];
+        double epc = delta / lum_meter[p];
+
+        res[p] = delta;
+
+        std::cerr << lum_meter[p] << " \t " << lc;
+        std::cerr << " \t " << delta << " \t " << epc * 100 << std::endl;
+    }
+
+    return res;
+
+}
+
 int main(int argc, char **argv) {
     namespace po = boost::program_options;
     using namespace iris;
@@ -43,12 +76,14 @@ int main(int argc, char **argv) {
     std::string input;
     std::string cones;
     double weight_exp = 1.1;
+    bool check_lum = false;
 
     po::options_description opts("calibration tool");
     opts.add_options()
             ("help", "produce help message")
             ("cone-fundamentals,c", po::value<std::string>(&cones)->required())
             ("weight-exponent,w", po::value<double>(&weight_exp))
+            ("check-luminance", po::value<bool>(&check_lum))
             ("input", po::value<std::string>(&input)->required());
 
     po::positional_options_description pos;
@@ -76,6 +111,7 @@ int main(int argc, char **argv) {
         std::cerr << "File missing spectra or patches" << std::endl;
         return 1;
     }
+
 
     h5x::DataSet sp = fd.openData("spectra");
     h5x::DataSet ps = fd.openData("patches");
@@ -159,7 +195,12 @@ int main(int argc, char **argv) {
     }
 
     caA.write(h5x::TypeId::Double , caA_dims, dklp.A);
-    fd.close();
 
+    if (check_lum) {
+        std::cerr << "Luminance check: " << std::endl;
+        cmp_luminance(fd, cf, spec);
+    }
+
+    fd.close();
     return 0;
 }
