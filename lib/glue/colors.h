08eadfd724b95ffcae57648bd6f6ac00cc6b3f3f
Christian Kellner <christian@kellner.me>
2015-03-26 09:33:10 +0100

    [glue] colors: remove rgb class

--- lib/glue/colors.h
+++ lib/glue/colors.h
@@ -8,32 +8,11 @@
 namespace glue {
 namespace color {
 
-struct rgb {
-
-    rgb() : r(0.0f), g(0.0f), b(0.0f) {}
-    rgb(float r, float g, float b) : r(r), g(g), b(b) {}
-
-    union {
-        struct {
-            float r;
-            float g;
-            float b;
-        };
-        struct {
-            float data[3];
-        };
-    };
-};
-
-
 struct rgba {
 
     rgba() : r(0.0f), g(0.0f), b(0.0f) {}
     rgba(float r, float g, float b) : rgba(r, g, b, 1.0f) {}
     rgba(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
-
-    rgba(const rgb &c) : r(c.r), g(c.g), b(c.b), a(1.0f) {}
-    operator rgb() const { return rgb(r, g, b); }
 
     #ifdef HAVE_IRIS
     rgba(const iris::rgb &o) : r(o.r), g(o.g), b(o.b), a(1.0f) { }
