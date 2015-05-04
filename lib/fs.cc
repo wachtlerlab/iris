29891f64d39a711d821906436db15086caa0d918
Christian Kellner <christian@kellner.me>
2015-05-04 15:24:04 +0200

    fs: dir_iterator: move implement to cc file

--- lib/fs.cc
+++ lib/fs.cc
@@ -1,2 +1,30 @@
 
 #include <fs.h>
+
+namespace fs {
+
+dir_iterator::dir_iterator(const file &fd) : dir_iterator(fd.path()) {
+}
+
+dir_iterator::dir_iterator(const std::string &path) :basepath(path), dirp(nullptr), entry(nullptr) {
+    DIR *dfd = opendir(basepath.c_str());
+    if (dfd) {
+        dirp = std::shared_ptr<DIR>(dfd, closedir);
+        next();
+    }
+}
+
+file dir_iterator::operator*() const {
+    return file(basepath).child(entry->d_name);
+}
+
+void dir_iterator::next() {
+
+    bool show_hidden = true;
+
+    do {
+        readdir_r(dirp.get(), &buffer, &entry);
+    } while(entry != nullptr && (!show_hidden && entry->d_name[0] == '.'));
+}
+
+}
--- lib/fs.h
+++ lib/fs.h
@@ -8,6 +8,59 @@
 
 namespace fs {
 
+class file;
+
+class dir_iterator {
+public:
+    typedef std::input_iterator_tag iterator_category;
+    typedef file value_type;
+    typedef ptrdiff_t difference_type;
+    typedef const value_type *pointer;
+    typedef const value_type &reference;
+
+    dir_iterator(const file &fd);
+    dir_iterator(const std::string &path);
+    dir_iterator() : basepath(), dirp(nullptr), entry(nullptr) { }
+
+
+    dir_iterator& operator++() {
+        next();
+        return *this;
+    }
+
+    dir_iterator operator++(int) {
+        dir_iterator tmp(*this);
+        ++(*this);
+        return tmp;
+    }
+
+    file operator*() const;
+
+    bool operator==(const dir_iterator &o) const {
+        if (entry == nullptr && o.entry == nullptr) {
+            return true;
+        } else if(entry != nullptr && o.entry != nullptr &&
+                  !strcmp(entry->d_name, o.entry->d_name)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool operator!=(const dir_iterator &o) const {
+        return !(*this == o);
+    }
+
+private:
+    void next();
+
+private:
+    std::string basepath;
+    std::shared_ptr<DIR> dirp;
+    struct dirent *entry;
+    struct dirent  buffer;
+};
+
 class file {
 
 public:
@@ -28,74 +81,6 @@ public:
 private:
     std::string loc;
 };
-
-class dir_iterator {
-public:
-    typedef std::input_iterator_tag iterator_category;
-    typedef file value_type;
-    typedef ptrdiff_t difference_type;
-    typedef const value_type *pointer;
-    typedef const value_type &reference;
-
-    dir_iterator(const file &fd) : basepath(fd.path()), dirp(nullptr), entry(nullptr) {
-
-        DIR *dfd = opendir(fd.path().c_str());
-        if (dfd) {
-            dirp = std::shared_ptr<DIR>(dfd, closedir);
-            next();
-        }
-    }
-
-    dir_iterator() : basepath(), dirp(nullptr), entry(nullptr) { }
-
-
-    dir_iterator& operator++() {
-        next();
-        return *this;
-    }
-
-    dir_iterator operator++(int) {
-        dir_iterator tmp(*this);
-        ++(*this);
-        return tmp;
-    }
-
-    file operator*() const {
-        return file(basepath).child(entry->d_name);
-    }
-
-    bool operator==(const dir_iterator &o) const {
-        if (entry == nullptr && o.entry == nullptr) {
-            return true;
-        } else if(entry != nullptr && o.entry != nullptr &&
-                  !strcmp(entry->d_name, o.entry->d_name)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    bool operator!=(const dir_iterator &o) const {
-        return !(*this == o);
-    }
-
-private:
-
-    void next() {
-
-        bool show_hidden = true;
-        do {
-            readdir_r(dirp.get(), &buffer, &entry);
-        } while(entry != nullptr && (!show_hidden && entry->d_name[0] == '.'));
-    }
-
-private:
-    std::string basepath;
-    std::shared_ptr<DIR> dirp;
-    struct dirent *entry;
-    struct dirent  buffer;
-};
-
 
 }
 
