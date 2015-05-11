720fe8b2661cc0dcae3fd9a664178625f9c1bfe2
Christian Kellner <christian@kellner.me>
2015-05-11 15:07:21 +0200

    cfg: base entity class for cfg objects

--- lib/cfg.h
+++ lib/cfg.h
@@ -13,6 +13,30 @@
 namespace iris {
 namespace cfg {
 
+struct entity {
+public:
+
+    entity() : id(""), rev(0) {}
+    entity(const std::string &id, uint64_t rev) : id(id), rev(rev) {}
+
+    const std::string &identifier() const { return id; }
+    uint64_t revision() const { return rev; };
+
+    std::string uid() const {
+        char buf[1024] = {0, };
+        snprintf(buf, sizeof(buf), "%llu@%s", rev, id.c_str());
+        return std::string(buf);
+    }
+
+    explicit operator bool() const { return !id.empty(); }
+
+
+private:
+    std::string id;
+    uint64_t    rev;
+};
+
+
 struct monitor {
     std::string id;
 
@@ -77,10 +101,10 @@ struct subject {
 
 /* possible dir layout:
  *     / monitors / __ID__ / device.monitor (or __ID__.monitor)
- *                         / 1024x1200@85:1.mode
- *                         / default.mode -> 1024x1200@85:1.mode [symlink]
- *   						/ __MODE__-DATETIME#1.rgb2lms
- *   						/ __MODE__-DATETIME#1.rgb2lms
+ *                         / 1024x1200:85$1.mode
+ *                         / default.mode -> 1024x1200:5$1.mode [symlink]
+ *   						/ __MODE__@DATETIME#1.rgb2lms
+ *   						/ __MODE__@DATETIME#1.rgb2lms
  *               / default -> __ID __ [symlink]
  */
 
