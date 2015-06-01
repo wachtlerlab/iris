db4ccad3d4dc3de7eaabc70f5ab961ca8c2f590b
Christian Kellner <christian@kellner.me>
2015-06-01 23:21:41 +0200

    [plot] ps: work with iso{slant, data} format

--- plot/iris-ps.py
+++ plot/iris-ps.py
@@ -3,41 +3,50 @@
 from __future__ import print_function
 from __future__ import division
 
-import h5py as h5
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import csv
 import argparse
-
+import yaml
+import csv
+import StringIO
 
 def main():
-    parser = argparse.ArgumentParser(description='CI - Configuration Tool')
+    parser = argparse.ArgumentParser(description='CI - Plot ISO fit')
     parser.add_argument('--freq', dest='freq', type=float, default=1.0)
+    parser.add_argument('--offset', type=float, default=0.667)
     parser.add_argument('data', type=str)
-    parser.add_argument('amp', type=float, default=None)
-    parser.add_argument('phase', type=float, default=None)
-    parser.add_argument('offset', type=float, default=0.66)
+    parser.add_argument('--iso', type=str, default=None)
 
     args = parser.parse_args()
-    x = []
-    y = []
-    with open(args.data, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='#')
-        for row in reader:
-            x.append(float(row[0]))
-            y.append(float(row[1]))
+    f = open(args.data)
+    data = yaml.safe_load(f)
+    f.close()
+
+    iso = None
+    if args.iso is not None:
+        f = open(args.iso)
+        iso = yaml.safe_load(f)
+        f.close()
+
+    raw = data['isodata']['data']
+    print(raw)
+    reader = csv.reader(StringIO.StringIO(raw), delimiter=',', quotechar='#')
+    x, y = zip(*[(float(r[0]), float(r[1])) for r in reader])
+    print(x, y)
 
     plt.scatter(x, y,  color='dodgerblue', label='data')
     plt.axhline(y=args.offset, color='black')
 
-    phase = args.phase
-    amp = args.amp
-
-    if phase is not None and amp is not None:
+    if iso is not None:
         x = np.array(sorted(set(x)))
         plt.hold(True)
-        yfit = args.offset + amp * np.cos(args.freq*x - phase)
+        freq = 1
+        amp = float(iso['isoslant']['dl'])
+        phase = float(iso['isoslant']['phi'])
+        print(amp, phase, x)
+        yfit = args.offset + amp * np.cos(x - phase)
         plt.plot(x, yfit, '--k')
 
     plt.show()
