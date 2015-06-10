58015094939247faba2bcd95def0d57737cae944
Christian Kellner <christian@kellner.me>
2015-06-10 18:01:42 +0200

    [tools] fitiso: fit offset cmd line switch
    Also some nicer output

--- tools/fitiso.cc
+++ tools/fitiso.cc
@@ -22,11 +22,13 @@ int main(int argc, char **argv) {
     std::string infile_path;
     bool fit_freq = false;
     bool only_stdout = false;
+    double offset = -1.0;
 
     po::options_description opts("calibration tool");
     opts.add_options()
             ("help", "produce help message")
             ("fit-frequency", po::value<bool>(&fit_freq), "also fit sin frequency [default=false]")
+            ("offset", po::value<double>(&offset), "fix the offset [default=fit it]")
             ("file", po::value<std::string>(&infile_path)->required())
             ("stdout", po::value<bool>(&only_stdout));
 
@@ -64,21 +66,20 @@ int main(int argc, char **argv) {
                        return s.response;
                    });
 
-    iris::sin_fitter fitter(x, y, fit_freq);
+    iris::sin_fitter fitter(x, y, fit_freq, offset);
     bool res = fitter();
 
     std::cerr << "success: " << res << std::endl;
-    std::cout << fitter.p[0] << " " << fitter.p[1] << " " << fitter.p[2] << " ";
-    if (fit_freq) {
-        std::cout << fitter.p[3];
-    }
+    std::cout << fitter.amplitude() << " " << fitter.phase() << " ";
+    std::cout << fitter.offset() << (offset < 0 ? " " : "* ");
+    std::cout << fitter.frequency() << (fit_freq ? " " : "* ");
     std::cout << std::endl;
 
     if (res) {
         std::string tstamp = iris::make_timestamp();
         iris::data::isoslant iso(input.identifier());
-        iso.dl = fitter.p[0];
-        iso.phi = fitter.p[1];
+        iso.dl = fitter.amplitude();
+        iso.phi = fitter.phase();
         iso.subject = input.subject;
         iso.display = input.display;
         iso.rgb2lms = input.rgb2lms;
