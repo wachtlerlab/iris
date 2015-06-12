61a2a971caf218b4915be764efb30b408c6dd98f
Christian Kellner <christian@kellner.me>
2015-06-12 16:47:22 +0200

    [tools] store: implement rgb2lms importing

--- tools/store.cc
+++ tools/store.cc
@@ -6,31 +6,116 @@
 #include <getopt.h>
 
 #include <iostream>
+#include <yaml-cpp/yaml.h>
 
-void cmd_info(int argc, char **argv) {
+static int cmd_info(int argc, char **argv) {
     iris::data::store store = iris::data::store::default_store();
 
     std::cout << "location: " << store.location().path() << std::endl;
     std::cout << "version: " << store.version_string() << std::endl;
 
-
-
+    return 0;
 }
 
+static int import_rgb2lms(iris::data::store &store,
+                          fs::file &fd,
+                          const std::string &data) {
+    iris::data::rgb2lms rgb2lms = store.yaml2rgb2lms(data);
+
+    fs::file f_imported = store.store_rgb2lms(rgb2lms);
+    fs::file base = f_imported.parent();
+    std::cerr << "[I] stored rgb2lms data [" << rgb2lms.identifier() << "]" << std::endl;
+
+    std::string root, ext;
+
+    std::tie(root, ext) = fd.splitext();
+
+    fs::file cac = fd.parent().child(root + ".cac");
+    fs::file msd = fd.parent().child(rgb2lms.dataset);
+
+    if (cac.exists()) {
+        std::cerr << "[I] found cac data: " << cac.path() << std::endl;
+        fs::file dest = base.child(cac.name());
+        if (dest.exists()) {
+            std::cerr << "[W] cac already exists. skipping!" << std::endl;
+        } else {
+            cac.copy(dest);
+            std::cerr << "[I] cac file imported!" << std::endl;
+        }
+    }
+
+    if (msd.exists()) {
+        std::cerr << "[I] found spectral data: " << msd.path() << std::endl;
+        fs::file dest = base.child(msd.name());
+        if (dest.exists()) {
+            std::cerr << "[W] spectral data file already exists. skipping! " << std::endl;
+        } else {
+            cac.copy(dest);
+            std::cerr << "[I] spectral data imported!" << std::endl;
+        }
+    }
+
+    return 0;
+}
+
+static int cmd_import(int argc, char **argv) {
+    iris::data::store store = iris::data::store::default_store();
+
+    static struct option longopts[] = {
+            { NULL,          0,                      NULL,           0 }
+    };
+
+    int ch;
+    while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1)
+        switch (ch) {
+            case '?':
+            default:
+                std::cerr << "unkown option" << std::endl;
+                std::cerr << "usage: import <object>" << std::endl;
+                return -1;
+        }
+
+    argc -= optind;
+    argv += optind;
+
+    if (argc != 1) {
+        std::cerr << "usage: import <object>" << std::endl;
+        return -1;
+    }
+
+    fs::file fd(argv[0]);
+    std::string data = fd.read_all();
+
+    YAML::Node doc = YAML::Load(data);
+    std::cerr << "[I] objects # in file: " << doc.size() << std::endl;
+
+    for (YAML::const_iterator it = doc.begin(); it !=doc.end(); ++it) {
+        std::string entity = it->first.as<std::string>();
+        std::cerr << "[I] importing: " << entity << std::endl;
+        if (entity == std::string("rgb2lms")) {
+            import_rgb2lms(store, fd, data);
+        } else {
+            std::cerr << "[W] cannot import this object. Skipping!" << std::endl;
+        }
+    }
+
+    return 0;
+}
 
 struct command {
     std::string name;
     std::string help;
-    void (*command_fn)(int argc, char **argv);
+    int (*command_fn)(int argc, char **argv);
 
-    void operator()(int argc, char **argv) const {
-        (*command_fn)(argc, argv);
+    int operator()(int argc, char **argv) const {
+        return (*command_fn)(argc, argv);
     }
 };
 
 command cmds[] = {
-        { "info", "general data store information", cmd_info },
-        {"",         "", nullptr}
+        { "info",   "general data store information", cmd_info },
+        { "import", "import data [rgb2lms, isoslant, ...] into store ", cmd_import },
+        { "",         "", nullptr}
 };
 
 static void usage(const std::string &binname = "") {
